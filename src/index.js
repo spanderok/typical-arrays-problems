5ba27b745b46fc83a857a52851987a1a9bc58a4c
Palyka Dima <polykodima@mail.ru>
2020-10-03 15:23:30 +0300

    init: finished task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined){
+    return 0;
+  }   if (array.length === 0){
+    return 0;
+  }  else {
+ return result = Math.min.apply(null, array);}
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined){
+    return 0;
+  }   if (array.length === 0){
+    return 0;
+  }  else {
+  return result = Math.max.apply(null, array);}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined){
+    return 0;
+  }   if (array.length === 0){
+    return 0;
+  }  else {
+  var sum = 0;
+  for( var i = 0; i < array.length; i++ ){
+  sum += parseInt( array[i], 10 ); //don't forget to add the base
+ };
+
+  var avg = sum/array.length;
+  return avg;}
+
 }
